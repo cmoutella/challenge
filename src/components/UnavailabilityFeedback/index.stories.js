1ffdcb1d9b086a192fa55d77c8703740c611f56a
Carolina Moutella <emaildacarolmoutella@gmail.com>
2023-03-06 18:08:26 -0300

    UnavailabilityFeedback: add story

---
+++ src/components/UnavailabilityFeedback/index.stories.js
@@ -0,0 +1,8 @@
+import UnavailabilityFeedback from '.'
+
+export default {
+  title: 'UnavailabilityFeedback',
+  component: UnavailabilityFeedback
+}
+
+export const Basic = () => <UnavailabilityFeedback />
