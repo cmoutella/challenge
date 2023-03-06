b9c95e50cf92b412a9d06b295174b1d2fa32744a
Carolina Moutella <emaildacarolmoutella@gmail.com>
2023-03-06 18:07:22 -0300

    PaymentDataForm: add story

--- src/components/PaymentDataForm/index.stories.js
+++ src/components/PaymentDataForm/index.stories.js
@@ -1,8 +1,34 @@
 import PaymentDataForm from '.'
+import { FormikProvider, useFormik } from 'formik'
+import { StoryWrapper } from './index.styles'
 
 export default {
   title: 'PaymentDataForm',
   component: PaymentDataForm
 }
 
-export const Basic = () => <PaymentDataForm />
+export const Default = () => {
+  const formik = useFormik({
+    isInitialValid: false,
+    initialValues: {
+      cardNumber: '',
+      cardExpirationDate: '',
+      ownerName: '',
+      ownerId: '',
+      coupon: '',
+      installments: '',
+      planId: '',
+      storeId: '',
+      gateway: '',
+      maxInstallments: 1
+    }
+  })
+
+  return (
+    <FormikProvider value={formik}>
+      <StoryWrapper>
+        <PaymentDataForm />
+      </StoryWrapper>
+    </FormikProvider>
+  )
+}
