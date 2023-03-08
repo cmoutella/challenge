9e1155fe6cba9d5f54d72c17515b0af51703a584
Carolina Moutella <emaildacarolmoutella@gmail.com>
2023-03-08 09:51:23 -0300

    PaymentFeedbackHeader: make it dynamic

--- src/components/PaymentFeedbackHeader/index.js
+++ src/components/PaymentFeedbackHeader/index.js
@@ -1,29 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faX } from '@fortawesome/free-solid-svg-icons'
 import {
   DetailedMessage,
   FeedbackMessage,
   FeedbackMessageWrapper,
   IconWrapper
 } from './index.styles'
-import useSubmitPayment from 'state/submitPayment'
+import { REQUEST_STATUS } from '../../state/requestStatus'
+import { useContext } from 'react'
+import { PaymentContext } from '../../provider/PaymentContext'
+
+const SUCCESS = {
+  title: 'Parabéns',
+  subtitle: 'Sua assinatura foi realizada com sucesso.'
+}
+const ERROR = {
+  title: 'Tivemos um problema',
+  subtitle:
+    'Infelizmente não pudemos finalizar sua assinatura agora, tente de novo mais tarde.'
+}
 
 const PaymentFeedbackHeader = () => {
-  const { feedback } = useSubmitPayment()
+  const payment = useContext(PaymentContext)
 
-  // if (!feedback) {
-  //   return null
-  // }
+  const displayMessage =
+    payment.status === REQUEST_STATUS.DONE ? SUCCESS : ERROR
+
+  const displayIcon = payment.status === REQUEST_STATUS.DONE ? faCheck : faX
 
   return (
     <FeedbackMessageWrapper>
       <IconWrapper>
-        <FontAwesomeIcon icon={faCheck} size="2x" />
+        <FontAwesomeIcon icon={displayIcon} size="2x" />
       </IconWrapper>
-      <FeedbackMessage>Parabéns</FeedbackMessage>
-      <DetailedMessage>
-        Sua assinatura foi realizada com sucesso.
-      </DetailedMessage>
+      <FeedbackMessage>{displayMessage.title}</FeedbackMessage>
+      <DetailedMessage>{displayMessage.subtitle}</DetailedMessage>
     </FeedbackMessageWrapper>
   )
 }
